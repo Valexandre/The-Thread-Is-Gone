c5749f52cd511167e053660841ad03f390ee605e
Valexandre <Valexandre@users.noreply.github.com>
2022-02-24 21:02:12 +0100

    Update script.R

--- script.R
+++ script.R
@@ -1,4 +1,11 @@
 library(rtweet)
-
+# On s'enregistre
+tweetbot_token <- rtweet::rtweet_bot(
+  api_key = Sys.getenv("T_API_KEY"),
+  api_secret = Sys.getenv("T_API_SECRET"),
+  access_token = Sys.getenv("T_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("T_ACCESS_SECRET")
+)
+rtweet::auth_as(tweetbot_token)
 rtweet::post_tweet(status = "Test")
 
