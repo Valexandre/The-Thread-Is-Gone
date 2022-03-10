63d3db6bf163b5429286f7b5d33732587c98ad26
Victor <Valexandre@users.noreply.github.com>
2022-03-10 21:05:32 +0100

    Test

--- script.R
+++ script.R
@@ -26,11 +26,17 @@ CreeMoiUnGradient<-colorRampPalette(c("#005F89", "white"))
 CouleursBornes<-rev(CreeMoiUnGradient(8))
 PrenomsDecedesParDepSexe<-readRDS("data/PrenomsDecedesParDepSexe.Rdata")
 
+
 sortunecartedesdecesparprenom<-function(PrenomS,DepDeDeces){
   tmpbdd<-PrenomsDecedesParDepSexe%>%
-    dplyr::filter(Prenom==PrenomS)%>%
-    dplyr::filter(DDD==DepDeDeces)%>%
-    left_join(NomsDep,by=c("DDD"="dep"))
+    filter(Prenom==PrenomS)%>%
+    filter(DDD==DepDeDeces)%>%
+    left_join(NomsDep,by=c("DDD"="dep"))%>%arrange(desc(PartDuPrenomNesDansDep))
+  TotalDecesPrenom<-PrenomsDecedesParDepSexe%>%
+    filter(Prenom==PrenomS)%>%
+    filter(Sexe==tmpbdd$Sexe[1])%>%
+    ungroup()%>%summarise(TotalPrenom=sum(Nombre))
+  round(TotalDecesPrenom$TotalPrenom,-2)
   
   Carte<-tmpbdd%>%
     left_join(DEPS,by=c("DDN"="INSEE_DEP"))%>%
@@ -39,16 +45,22 @@ sortunecartedesdecesparprenom<-function(PrenomS,DepDeDeces){
     geom_sf(aes(fill=PartDuPrenomNesDansDep),colour="lightgray")+
     geom_sf(data=DEPS, fill=NA,aes(colour=INSEE_DEP==DepDeDeces))+
     scale_colour_manual("",values=c("lightgray","black"))+
-    theme_void()+
+    theme_map()+
     labs(title=str_wrap(paste0("Où sont nées les personnes prénommées ",str_to_title(PrenomS)," décédées ",unique(tmpbdd$LieuDep) ," entre 2019 et 2021?"),40),
-         subtitle=str_wrap(paste0("Part des ",sum(tmpbdd$Nombre)," personnes décédées ",unique(tmpbdd$LieuDep), " selon leur département de naissance."),65),  caption="Données Insee, calculs & carte V.Alexandre @humeursdevictor")+
+         subtitle=str_wrap(paste0("Part des ",sum(tmpbdd$Nombre)," personnes décédées ",unique(tmpbdd$LieuDep), " selon leur département de naissance."),65),  caption="Données Insee (Fichier des personnes décédées), calculs & carte V.Alexandre @humeursdevictor")+
     scale_fill_stepsn("", colours=CouleursBornes,breaks=c(0,2.5,5,7.5,10,20,30,50,100),limits=c(0,100),labels = function(x) {x}, show.limits = F,values = c(0/100,2.5/100,5/100,7.5/100,10/100,20/100,30/100,50/100,1))+
     guides(colour=FALSE,fill=guide_colorsteps(barwidth = 30, barheight = 2,even.steps = T))+
     theme(legend.position="top",plot.title.position = "plot",
           text=element_text(family = "Corbel",size=24))
-  agg_png(paste0(Sys.Date(),"_Carte_",PrenomS,"_",DepDeDeces,".png"), width = 900, height = 900, res = 144)
+  NomCarte<-paste0("img/",Sys.Date(),"_Carte_",PrenomS,"_",DepDeDeces,".png")
+  agg_png(NomCarte, width = 900, height = 900, res = 144)
   plot(Carte)
   invisible(dev.off())
+  
+  TW1<-paste0("Entre 2019 et 2021, près de ",round(TotalDecesPrenom$TotalPrenom,-2)," personnes prénommées ",str_to_title(PrenomS)," sont mortes en France. ", sum(tmpbdd$Nombre)," ",str_to_title(PrenomS)," sont ",ifelse(tmpbdd$Sexe[1]=="1","décédés ","décédées "),tmpbdd$LieuDep[1],". ",Virg(round(tmpbdd$PartDuPrenomNesDansDep[1])),"% d'entre ", ifelse(tmpbdd$Sexe[1]=="1","eux y étaient nés.","elles y étaient nées."))
+  
+  rtweet::post_tweet(statut=TW1,media = NomCarte,
+                     media_alt_text = paste0("Carte des ",str_to_title(PrenomS)," ",ifelse(tmpbdd$Sexe[1]=="1","décédés ","décédées "),tmpbdd$LieuDep[1]," entre 2019 et 2021."), token = tweetbot_token)
 }
   
 sortunecartedesdecesparprenom(sample(PrenomsDecedesParDepSexe$Prenom[PrenomsDecedesParDepSexe$Sexe==sample(1:2,1)],1),sample(DEPS$INSEE_DEP,1))
