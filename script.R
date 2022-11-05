99128c87c141b204bbc1a51805c36f1de2814e37
Victor <Valexandre@users.noreply.github.com>
2022-11-05 07:26:03 +0100

    Gt

--- script.R
+++ script.R
@@ -185,7 +185,7 @@ SortLeTop3<-function(langue){
   PagesPlusVues<-PagesPlusVues[!grepl(":",PagesPlusVues$article),]
   PagesPlusVues<-PagesPlusVues%>%mutate(Titre=gsub("_"," ",article))
   PagesPlusVues<-PagesPlusVues%>%
-    filter(Titre%!in%c("Main Page","Pagina principale","Accueil","Hoofdpagina","Página_principal","Cookie (informatique)","Cleopatra","Cleópatra","AMBEV" ))%>%
+    filter(Titre%!in%c("Main Page","Pagina principale","Accueil","Hoofdpagina","Página_principal","Cookie (informatique)","Cleopatra","Cleópatra","AMBEV","Google Traduction" ))%>%
     mutate(rank=row_number())%>%filter(!is.na(Titre))
   PagesPlusVues<-PagesPlusVues[1:3,1:4]
   PagesPlusVues%>%mutate(langue=langue,
